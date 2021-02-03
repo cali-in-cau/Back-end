f4bc40df695f26ddd120789e09e1f98e803651e8
freemjstudio <mjwoo0@naver.com>
2021-02-03 21:57:52 +0900

    install newsapi

--- autoTA/stocks/urls.py
+++ autoTA/stocks/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    #path('search/',views.search_stock, name='search'),
     path('search/<str:keyword>',views.search_stock),
     path('search/<str:stock_code>',views.search_stock_with_code),
     path('save_all/',views.save_stocks),
