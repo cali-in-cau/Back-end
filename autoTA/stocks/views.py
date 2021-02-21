501747d1335c6ad850892e87caed2a7dc486a0cc
eziong <bsjwoong@gmail.com>
2021-02-21 14:20:08 +0900

    back -> ml dataset fixed

--- autoTA/stocks/views.py
+++ autoTA/stocks/views.py
@@ -39,13 +39,17 @@ def stock_graph(request):
 # front -> back 으로 predict 정보를 요청할 때
 def stock_predict(request):
     date_type = request.GET.get('date_type')
-    start_date = request.GET.get('start_date')
     stock_code = request.GET.get('stock_code')
-    print(date_type,start_date,stock_code)
-    data = stocks_api.get_stock_data(stock_code,date_type,start_date)
+    data = None
+    if(date_type=='day'):
+        data = stocks_api.get_stock_data(stock_code,date_type,1)
+    if(date_type=='week'):
+        data = stocks_api.get_stock_data(stock_code,date_type,4)
+    if(date_type=='month'):
+        data = stocks_api.get_stock_data(stock_code,date_type,12)
     data = json.dumps(data, cls=DjangoJSONEncoder,ensure_ascii = False)
-    res = requests.post('http://yaeoni.o-r.kr/ml/predict', data)
-    result = {'predict':json.loads(res.text),'data':json.loads(data)}
+    res = requests.post('http://localhost:8888/ml/predict', data)
+    result = json.loads(res.text)
     result = json.dumps(result, cls=DjangoJSONEncoder,ensure_ascii = False)
     return HttpResponse(result)
 
