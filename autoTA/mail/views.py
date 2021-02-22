a32d22557f6e79a8e3cc9dee64362b5a973bd429
freemjstudio <mjwoo0@naver.com>
2021-02-22 10:57:46 +0900

    add mail

--- autoTA/mail/views.py
+++ autoTA/mail/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, HttpResponse
 from django.core.mail import send_mail as sm
 from django.conf import settings
 from users.models import User
+from users.models import Favorite
+from stocks.models import Stock
+from autoTA.utils import stocks_api
 # Create your views here.
 
 def send_mail(request):
@@ -9,21 +12,29 @@ def send_mail(request):
     user_data = User.objects.all() # user saved in DataBase
     user_list = []
 
+    user_favorite = Favorite.objects.values('stock','user')
+    favorites = {} # {user:stock lists}
+    user_favorite_stocks_name = []
+
+    stock_data = Stock.objects.all()
+
     for user in user_data:
         user_list.append(user.email)
+        stock_lists = user_favorite.filter(user = user)
+        # for stock in stock_lists:
+        #     stock.stock_name
+        #     # user_favorite_stocks_name.append(stock.stock_name)
+        favorites[user.email] = stock_lists
 
-    res = sm(
-        subject = '[StockReader] Time to check your stock at StockReader',
-        message = 'Time to sell the stock!! Visit StockReader website and get more details.',
-        from_email = settings.EMAIL_HOST_USER,
-       # recipient_list = user_list,
-        recipient_list = [
-            'mjwoo0@naver.com'
-        ],
-        fail_silently = False
-    )
+    for user_email in favorites.keys():
 
-    return HttpResponse(f"Email sent to {res} members")
+        res = sm(
+            subject = '[StockReader] Time to read your stock graph',
+            message = f'Time to check your stocks! {favorites[user_email]} : BULL ',
+            from_email = settings.EMAIL_HOST_USER,
+            recipient_list = [user_email],
+            fail_silently = False
+        )
 
-def mail(request, state):
 
+    return HttpResponse(f"Email sent to members")
